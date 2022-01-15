549bcf3b8798e09e5e95e041f5ecbd97bd43dd98
Ken V <ken@kverb.com>
2022-01-15 11:02:36 -0500

    [fish] attempt EDITOR fix

--- .config/fish/conf.d/custom.fish
+++ .config/fish/conf.d/custom.fish
@@ -4,3 +4,5 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 function dotfiles --wraps='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' --description 'alias dotfiles=/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv;
 end
+
+set --universal EDITOR nvim
