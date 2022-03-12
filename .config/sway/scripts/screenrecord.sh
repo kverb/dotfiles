21279ddcc98f784ec4c3de7e6efb61aba3905063
Ken V <ken@kverb.com>
2022-03-12 10:14:30 -0500

    [sway] add screenshot and screenrecording scripts

---
+++ .config/sway/scripts/screenrecord.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+wf-recorder -g "$(slurp)" -f ~/Videos/screencap.mp4
+# entries="Active Screen Output Area Window"
+#  
+# selected=$(printf '%s\n' $entries | wofi --style=$HOME/.config/wofi/style.widgets.css --conf=$HOME/.config/wofi/config.screenshot | awk '{print tolower($1)}')
+#  
+# case $selected in
+#   active)
+#     /usr/share/sway/scripts/grimshot --notify copy active;;
+#   screen)
+#     /usr/share/sway/scripts/grimshot --notify copy screen;;
+#   output)
+#     /usr/share/sway/scripts/grimshot --notify copy output;;
+#   area)
+#     /usr/share/sway/scripts/grimshot --notify copy area;;
+#   window)
+#     /usr/share/sway/scripts/grimshot --notify copy window;;
+# esac
