1bf5149116833edd5b3d6b7b08498bba0880fecc
Ken V <git@kverb.com>
2023-10-06 11:12:57 -0400

    [sway][waybar] encom-theme powermenua and organize launcher
    1. Changes the "powermenu" from wofi snippet to be encom theme, using
       fuzzel (for both waybar power icon and kb shorcut in sway.
    2. Moves the 'launcher menu' code into its own config file for sway.

--- .config/waybar/scripts/power-menu.sh
+++ .config/waybar/scripts/power-menu.sh
@@ -2,7 +2,28 @@
 
 entries="Logout Suspend Reboot Shutdown"
 
-selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
+# selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
+
+selected=$(printf '%s\n' $entries | fuzzel \
+  -d \
+  -R \
+  -l 4 \
+  -p ' '        \
+  -x 20          \
+  -r 0           \
+  -b 000000ff    \
+  -t 00a595ff    \
+  -s 00a48cff    \
+  -S 000000ff    \
+  -C 00a595ff    \
+  -B 2           \
+  -m bc00caff    \
+  -M f09a1aff    \
+  -w 60          \
+  --show-actions \
+  --line-height=20 \
+  -f "Spleen 32x64:size=14" \
+  | awk '{print tolower($1)}')
 
 case $selected in
   logout)
